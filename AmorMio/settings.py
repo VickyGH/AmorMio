4f74f411b6dedcd05178ed22794778e9ef9241f6
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2017-08-29 21:55:05 -0500

    Cambio IP

--- AmorMio/settings.py
+++ AmorMio/settings.py
@@ -17,7 +17,7 @@ from os.path import normpath, join
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'AmorMio.herokuapp.com']
+ALLOWED_HOSTS = ['AmorMio.herokuapp.com']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'AmorMio.urls'
