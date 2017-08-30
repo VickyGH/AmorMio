87299244471179f1649f042acbe1b7f6208b1d24
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2017-08-29 20:37:07 -0500

    Estaticos Heroku

--- AmorMio/urls.py
+++ AmorMio/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     url(r'^VGH/', admin.site.urls),
     url(r'^Administrador/', include('Apps.Administrador.urls', namespace='Admin')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
