9d4aa435453004446801556d7d9f2ab936a9882b
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2017-10-15 15:16:51 -0500

    checando los staticos 2

--- AmorMio/urls.py
+++ AmorMio/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^VGH/', admin.site.urls),
     url(r'^Administrador/', include('Apps.Administrador.urls', namespace='Admin')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.PROJECT_ROOT, document_root=settings.PROJECT_ROOT)
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
